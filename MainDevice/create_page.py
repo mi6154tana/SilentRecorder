af5018cae153de2e32bbd7046fb3240b32e38305
mi6154tana <mi6154tana@g.kumamoto-nct.ac.jp>
2019-09-14 11:09:00 +0900

    任意のディレクトリからその中のファイル名を抽出し、リストに表示可能

--- MainDevice/create_page.py
+++ MainDevice/create_page.py
@@ -3,12 +3,13 @@ import tkinter as tk
 from tkinter import ttk
 from time import sleep
 from PIL import Image, ImageTk
+import os
 
 class CreatePage:
     p_frame = None
     p_name = None
     cv = None
-    def __init__(self, root, name, contents):
+    def __init__(self, root, name, p_num):
         self.p_frame = tk.Frame(root)
         self.p_name = name
         #キャンバスを作る
@@ -16,21 +17,39 @@ class CreatePage:
         #self.cv.create_rectangle(0, 0, 800, 450, fill = 'green')#塗りつぶし
         #キャンバスバインド
         self.cv.place(x=0, y=0)
-        self._create_cons(root, contents)
+        self._create_cons(root, p_num)
         #self.p_frame.pack()
         self.p_frame.grid(row=0, column=0, sticky="nsew")
 
-    def _create_cons(self, root, contents):
+    def _create_cons(self, root, p_num):
         name_label_font  = ("Helevetice", 14)
-        name_label = ttk.Label(self.p_frame, text = self.p_name, font = name_label_font)
+        name_label = tk.Label(self.p_frame, text = self.p_name, font = name_label_font)
         name_label.grid(row=4, column=0)
+        cons_lavels = []
 
-        self.cv.create_polygon(10,10, 20,10, 20,20, 10,20, fill = 'red',)
+        contents = self.get_list_cons(p_num)
         if len(contents) == 0:
             print('No contents')
         for i in range(len(contents)):
             print(contents[i])
-            self.cv.create_polygon(10,10+i*50, 500,10+i*50, 500,50+i*50, 10,50+i*50, fill = 'red',)
+            self.cv.create_polygon(10,50+i*50, 500,50+i*50, 500,90+i*50, 10,90+i*50, fill = 'blue',)
+            cons_lavels.append(tk.Label(self.p_frame, text = contents[i],background = "blue",font = ("",20,"bold")))
+            cons_lavels[i].place(x = 10, y = 50+i*50)
+
+    def get_list_cons(self, p_num):
+        if p_num == 0:
+            return ['通常演奏', '正確性診断', '記録', '設定']
+        elif p_num == 2 or p_num == 4:
+            if p_num == 2:
+                path = './Score'
+            else:
+                path = './Memory'
+            files = os.listdir(path)
+            files_file = [f for f  in files if os.path.isfile(os.path.join(path, f))]
+
+            return files_file
+        else:
+            return ['None']
 
     def raise_page(self):     
         self.p_frame.tkraise()
