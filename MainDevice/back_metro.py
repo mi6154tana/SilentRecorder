dd1a1a92499677e04a318726888947d4dc0a6d29
mi6154tana <mi6154tana@g.kumamoto-nct.ac.jp>
2019-09-24 13:52:20 +0900

    メトロノームの音のタイミングを正確にした

--- MainDevice/back_metro.py
+++ MainDevice/back_metro.py
@@ -11,6 +11,7 @@ class BackMetro:
 
     def __init__(self):
         pygame.mixer.quit()
+        pygame.mixer.pre_init(44100,-16,1,512)
         pygame.mixer.init()
 
         self.mflag = 0
@@ -23,12 +24,12 @@ class BackMetro:
         self.sound_metro_2 = pygame.mixer.Sound(self.metro_fname_1)
 
     def _play_metronome(self, tempo):
-        last_time = time.time()
+        last_time = time.perf_counter()
         ob_flag = 0
         while 1:
             if self.mflag == 0:
                 break
-            now_time = time.time()
+            now_time = time.perf_counter()
             interval = now_time - last_time
             #print('BPM : ' + str(tempo))
             if interval >= tempo:
@@ -36,12 +37,14 @@ class BackMetro:
                 if ob_flag == 0:
                     #self.sound_metro_2.stop()
                     self.sound_metro_1.play()
+                    #print("1:",self.sound_metro_1.get_length())
                 elif ob_flag == 1:
                     #self.sound_metro_1.stop()
                     self.sound_metro_2.play()
+                    #print("2:",self.sound_metro_2.get_length())
                 ob_flag = 1- ob_flag
-                #time.sleep(self.sound_metro_1.get_length())
-                last_time = time.time()
+                    #time.sleep(self.sound_metro_1.get_length())
+                last_time = time.perf_counter()
 
     def metro_start(self, bpm):
         self.mflag = 1
@@ -54,9 +57,11 @@ class BackMetro:
     def metro_stop(self):
         self.mflag = 0
 
+
 def main():
-    bpm = 120
+    bpm = 60
     metro = BackMetro()
+    #metro.s()
     metro.metro_start(bpm)
     #BackMetronome(bpm)
 
